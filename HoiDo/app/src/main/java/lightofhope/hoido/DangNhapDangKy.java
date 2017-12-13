9cc751ac2d860412441da6bf668f5b27b8454819
root <root@lulu>
2017-12-13 09:13:26 -0500

    Them man hinh dang nhap va dang ky

---
+++ HoiDo/app/src/main/java/lightofhope/hoido/DangNhapDangKy.java
@@ -0,0 +1,24 @@
+package lightofhope.hoido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DangNhapDangKy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap_dang_ky);
+
+        addControl();
+        addEvent();
+    }
+
+    public void addControl(){
+
+    }
+
+    public void addEvent() {
+
+    }
+}
--- HoiDo/app/src/main/java/lightofhope/hoido/MainActivity.java
+++ HoiDo/app/src/main/java/lightofhope/hoido/MainActivity.java
@@ -1,13 +1,39 @@
 package lightofhope.hoido;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControl();
+        addEvent();
+    }
+
+    public void addControl(){
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+    }
+
+    public void addEvent() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(this, "", Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(MainActivity.this, DangNhapDangKy.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
+
